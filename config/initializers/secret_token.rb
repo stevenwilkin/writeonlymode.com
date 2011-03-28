a6fabbd538fb96bd8ec9adfad7b096c5269901f4
Steven Wilkin <stevenwilkin@gmail.com>
2011-03-28 21:41:13 +0100

    add freshly generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WriteonlymodeCom::Application.config.secret_token = '356a626dd6ef5a09fe916a96ba99331b622b77828fd5c48a3fa97f465f4227a7961eb991ec853487f56d2ae179d25c826f2a8ab03a5f7226fc485cf999ba3950'
