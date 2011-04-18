d4f4f6e95664aa0d30886f44d4c616fe0b6a0aff
Steven Wilkin <stevenwilkin@gmail.com>
2011-04-18 23:21:52 +0100

    add basics of Twitter OAuth authentication using omniauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do  
+  provider :twitter, 'llyJM02BwgKpYi1p82u14w', 'JzvuiMMztb5PUrGUC3oAb6HCUE4r9ecQ8GjK7cFXBLs'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 WriteonlymodeCom::Application.routes.draw do
   root :to => 'statuses#index'
+  match '/auth/:provider/callback' => 'sessions#create'
 end
